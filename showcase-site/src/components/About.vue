f33033ff2d8e7ac41104b6d3d69fe5092100d227
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-09-16 10:00:59 +0200

    feature : add about component

---
+++ showcase-site/src/components/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="about">
+      <h2>🌍 About Me</h2>
+      <p>{{ about }}</p>
+    </section>
+</template>
+  
+<script>
+  export default {
+    name: 'AppAbout',
+    props: {
+      about: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+  
+<style scoped lang="scss">
+  .about {
+    margin-bottom: 2rem;
+  
+    h2 {
+      margin-bottom: 1rem;
+    }
+  
+    p {
+      text-align: justify;
+    }
+  }
+</style>
