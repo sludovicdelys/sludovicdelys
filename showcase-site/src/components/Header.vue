761b088a11bee9ad794ac877fc76e5fdde34489a
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-09-16 09:52:12 +0200

    style : add header component

---
+++ showcase-site/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+      <h1>{{ name }}</h1>
+      <p>{{ title }}</p>
+    </header>
+</template>
+  
+<script>
+  export default {
+    name: 'AppHeader',
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+  
+  <style scoped lang="scss">
+  header {
+    text-align: center;
+    margin-bottom: 2rem;
+  
+    h1 {
+      margin-bottom: 0.5rem;
+    }
+  
+    p {
+      font-size: 1.2rem;
+      color: var(--secondary-color);
+    }
+  }
+  </style>
---
+++ showcase-site/src/views/Portfolio.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="portfolio">
+      <Header :name="data.name" :title="data.title" />
+      <About :about="data.about" />
+      <Skills :skills="data.skills" />
+      <WorkInProgress :workInProgress="data.workInProgress" />
+      <Projects :projects="data.projects" />
+      <DeployedProjects :deployedProjects="data.deployedProjects" />
+      <Aspirations :aspirations="data.aspirations" />
+      <Contact :contactInfo="data.contactInfo" :linkedInUrl="data.linkedInUrl" />
+      <Footer :footerText="data.footerText" />
+    </div>
+</template>
+  
+<script>
+  import Header from '@/components/Header.vue'
+  import About from '@/components/About.vue'
+  import Skills from '@/components/Skills.vue'
+  import WorkInProgress from '@/components/WorkInProgress.vue'
+  import Projects from '@/components/Projects.vue'
+  import DeployedProjects from '@/components/DeployedProjects.vue'
+  import Aspirations from '@/components/Aspirations.vue'
+  import Contact from '@/components/Contact.vue'
+  import Footer from '@/components/Footer.vue'
+  import portfolioData from '@/data/portfolioData.js'
+  
+  export default {
+    name: 'AppPortfolio',
+    components: {
+      Header,
+      About,
+      Skills,
+      WorkInProgress,
+      Projects,
+      DeployedProjects,
+      Aspirations,
+      Contact,
+      Footer
+    },
+    data() {
+      return {
+        data: portfolioData
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .portfolio {
+    @extend .container;
+  }
+</style>
