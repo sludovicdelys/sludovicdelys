6e74414731bce97355eaabdb56e32bda3d6414cf
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-09-16 10:02:24 +0200

    feature : add skills component

---
+++ showcase-site/src/components/Skills.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="skills">
+      <h2>🧰 Technologies and Environments</h2>
+      <ul>
+        <li v-for="(skillSet, category) in skills" :key="category">
+          <strong>{{ category }}:</strong> {{ skillSet.join(', ') }}
+        </li>
+      </ul>
+    </section>
+</template>
+  
+<script>
+  export default {
+    name: 'AppSkills',
+    props: {
+      skills: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+  
+<style scoped lang="scss">
+  .skills {
+    margin-bottom: 2rem;
+  
+    h2 {
+      margin-bottom: 1rem;
+    }
+  
+    ul {
+      list-style-type: none;
+      padding-left: 0;
+    }
+  
+    li {
+      margin-bottom: 0.5rem;
+    }
+  }
+</style>
