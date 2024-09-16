8d462dc5362b16c2986a8b3dd634f4c43f451fe1
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-09-16 10:06:39 +0200

    feature : add aspirations component

---
+++ showcase-site/src/components/Aspirations.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="aspirations">
+      <h2>✨ Professional Aspirations</h2>
+      <p>{{ aspirations }}</p>
+    </section>
+</template>
+  
+<script>
+  export default {
+    name: 'AppAspirations',
+    props: {
+      aspirations: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+  
+<style scoped lang="scss">
+  .aspirations {
+    margin-bottom: 2rem;
+  
+    h2 {
+      margin-bottom: 1rem;
+    }
+  
+    p {
+      font-style: italic;
+      text-align: justify;
+    }
+  }
+</style>
