f72522823252d57e0d4c2cb8ac8ce3fc4266fa41
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-09-16 10:09:15 +0200

    add footer component

---
+++ showcase-site/src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="footer">
+      <p>{{ footerText }}</p>
+    </footer>
+</template>
+  
+<script>
+  export default {
+    name: 'AppFooter',
+    props: {
+      footerText: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+  
+<style scoped lang="scss">
+  .footer {
+    margin-top: 2rem;
+    text-align: center;
+    font-style: italic;
+    border-top: 1px solid var(--secondary-color);
+    padding-top: 1rem;
+  }
+</style>
